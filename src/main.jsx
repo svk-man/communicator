50268b904feab63da47853bbc174ca99de898047
Artem <awrer1234eads@gmail.com>
2022-03-30 16:36:55 +0300

    pulls test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Welcome />
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+function Welcome() {
+  return (
+    <div>
+      Hello, world!
+    </div>
+  )
+}
