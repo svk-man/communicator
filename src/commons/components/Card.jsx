da5d56b0d1adafacab2a9e0c694a90b24af59e86
Artem <awrer1234eads@gmail.com>
2022-04-09 21:56:05 +0300

    create favourite button

--- src/commons/components/Card.jsx
+++ src/commons/components/Card.jsx
@@ -1,19 +1,56 @@
+import { useEffect, useState } from 'react';
+import store from '../../store/store';
+import { addToFavourites, removeFromFavourites } from '../../store/actions';
 import '../assets/styles/card.css';
+import { playSound } from '../audio';
+import LOCAL_STORAGE from '../../store/localStorage';
 
 function Card(props) {
-  const { name, image } = props;
+  const { id, name, image } = props;
+  const [checked, setChecked] = useState( false );
+
+  function handleFavourite() {
+    if ( !checked ) {
+      store.dispatch( addToFavourites({
+        id,
+        name,
+        image,
+      }) )
+    } else {
+      store.dispatch( removeFromFavourites({
+        id,
+        name,
+        image,
+      }) )
+    };
+
+    setChecked( !checked );
+  }
+
+  useEffect(() => {
+    const newState = store.getState()?.favourites || LOCAL_STORAGE.getFavouriteCards() || [];
+
+    LOCAL_STORAGE.saveFavouriteCards( newState );
+  }, [checked]);
+
+  useEffect(() => {
+    const isInStorage = LOCAL_STORAGE.getFavouriteCards().some( card => card.id === id );
+
+    setChecked( isInStorage );
+  }, [id]);
 
   return (
     <div className="card">
       <div className="card__interaction">
-        <button className="card__sound" />
+        <button className="card__sound" onClick={ () => playSound( name ) } />
         <img className="card__image" src={ image } alt={ name } />
+        <input className="card__favourite" type="checkbox" checked={ checked } onChange={ handleFavourite } />
       </div>
       <h4 className="card__name">
         { name }
       </h4>
     </div>
-  )
+  );
 }
 
 export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/components/Home';
-import CardSearchPage from './pages/card-search-page/components/CardSearchPage';
+import CardSearchPage from './pages/card-search/components/CardSearchPage';
 import UndefinedPage from './pages/undefined/components/UndefinedPage';
 
 ReactDOM.render(
